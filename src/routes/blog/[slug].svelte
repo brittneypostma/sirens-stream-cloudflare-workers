d806cf2c4c9efd2061ca521fc6ead0cdfd602d9a
brittneypostma <brittney.postma@hotmail.com>
2022-03-30 12:06:15 -0400

    removed credentials

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,8 +1,6 @@
 <script context="module">
 	export async function load({ params, fetch }) {
-		const res = await fetch(`/blog/${params.slug}.json`, {
-			credentials: 'omit'
-		});
+		const res = await fetch(`/blog/${params.slug}.json`);
 		const data = await res.json();
 		return {
 			props: data,
